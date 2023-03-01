6aa148a781bee721c90c1746e8d2499d372fd7da
zaryabdev <zaryabdev404@gmail.com>
2023-03-01 17:48:57 +0500

    added: layout

--- my-project/src/App.jsx
+++ my-project/src/App.jsx
@@ -1,13 +1,5 @@
 function App() {
-    return (
-        <div className="flex text-white h-screen">
-            <div className="bg-gray-800 p-4">
-                <div className="bg-white text-gray-800 w-12 h-12">TZ</div>
-            </div>
-            <div className="bg-gray-700 p-4">Channels</div>
-            <div className="bg-gray-600 p-4 flex-1 ">Main</div>
-        </div>
-    );
+    return <div className="flex bg-black text-white h-screen">hi</div>;
 }
 
 export default App;
---
+++ my-project/src/Layout.jsx
@@ -0,0 +1,21 @@
+function App() {
+    return (
+        <div className="flex text-white h-screen">
+            <div className="bg-gray-800 p-4">
+                <div className="bg-white text-gray-800 w-12 h-12 rounded-full flex items-center justify-center">
+                    TZ
+                </div>
+            </div>
+            <div className="bg-gray-700  w-60 flex flex-col">
+                <div className="p-4 shadow-md">Tailwind CSS</div>
+                <div className="p-4 flex-1">Channel</div>
+            </div>
+            <div className="bg-gray-600 flex-1 flex flex-col ">
+                <div className="p-4 shadow-md">Tailwind CSS</div>
+                <div className="p-4 flex-1">Channel</div>
+            </div>
+        </div>
+    );
+}
+
+export default App;
